005726ba2665c8e8df8fc9e0ff1908e715a2143b
Rahul <dasr16583@gmail.com>
2023-03-01 01:02:53 +0530

    added banner

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,16 +4,20 @@ import axios from "axios";
 
 const apiKey = "f5bcb9699ca9b7e7a0eac288bd4ec870";
 const url = "https://api.themoviedb.org/3/movie";
+const imgUrl = "https://image.tmdb.org/t/p/original";
 const upComing = "upcoming";
+const nowplaying = "now_playing";
+const popular = "popular";
+const toprated = "top_rated";
 
 const Card = ({ img }) => <img className="card" src={img} alt="cover" />;
 
 const Row = ({
   title,
   arr = [
-    {
-      img: "https://wallpapers.com/images/featured-full/avengers-endgame-mghdp4gaqzu4q4us.jpg",
-    },
+    // {
+    //   img: "https://wallpapers.com/images/featured-full/avengers-endgame-mghdp4gaqzu4q4us.jpg",
+    // },
   ],
 }) => (
   <div className="row">
@@ -28,34 +32,69 @@ const Row = ({
     <Card img={ "https://wallpapers.com/images/featured-full/avengers-endgame-mghdp4gaqzu4q4us.jpg"} /> */}
 
       {arr.map((item, index) => (
-        <Card key={index} img={item.img} />
+        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
       ))}
     </div>
   </div>
 );
 
 const Home = () => {
-  const [upcoming, setupcoming] = useState([]);
+  const [upcomingMovies, setupcomingMovies] = useState([]);
+  const [nowplayingMovies, setnowplayingMovies] = useState([]);
+  const [popularMovies, setpopularMovies] = useState([]);
+  const [topratedMovies, settopratedMovies] = useState([]);
 
   useEffect(() => {
     const fecthUpcoming = async () => {
       const {
         data: { results },
       } = await axios.get(`${url}/${upComing}/?api_key=${apiKey}`);
-      setupcoming(results);
+      setupcomingMovies(results);
     };
 
+    const fecthNowplaying = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/${nowplaying}/?api_key=${apiKey}`);
+      setnowplayingMovies(results);
+    };
+
+    const fecthPopular = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/${popular}/?api_key=${apiKey}`);
+      setpopularMovies(results);
+    };
+
+    const fecthToprated = async () => {
+      const {
+        data: { results },
+      } = await axios.get(`${url}/${toprated}/?api_key=${apiKey}`);
+      settopratedMovies(results);
+    };
+
+    // function call
     fecthUpcoming();
+    fecthNowplaying();
+    fecthPopular();
+    fecthToprated();
   }, []);
 
   return (
     <section className="home">
-      <div className="banner"></div>
+      <div
+        className="banner"
+        style={{
+          backgroundImage: upcomingMovies[5]
+            ? `url(${`${imgUrl}/${upcomingMovies[5].poster_path}`})`
+            : "rgb:(16,16,16)",
+        }}
+      ></div>
 
-      <Row title={"popular on netflix"} />
-      <Row title={"TV Shows"} />
-      <Row title={"Recently Viwed"} />
-      <Row title={"My List"} />
+      <Row title={"Upcoming Movies"} arr={upcomingMovies} />
+      <Row title={"Now playing Movies"} arr={nowplayingMovies} />
+      <Row title={"Populer Movies"} arr={popularMovies} />
+      <Row title={"Toprated Movies"} arr={topratedMovies} />
     </section>
   );
 };
